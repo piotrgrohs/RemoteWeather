9875189933ae0cae46266f64f78f59830395241e
piotrgrohs <piotrgrohs@gmail.com>
2019-06-22 14:15:23 +0200

    v2.0

--- Main.ino
+++ Main.ino
@@ -6,34 +6,34 @@
 #include <Adafruit_BME280.h>
 #define SEALEVELPRESSURE_HPA (1013.25)
 
-Adafruit_BME280 bme; // I2C 22 na 4 i 21 na 2
+Adafruit_BME280 bme; 
 LiquidCrystal lcd(4,23,5,18,19,2);
 int LED_BUILTIN=2;
-
+IPAddress IP;
 bool status;
 String header;
 int counter = 0;
+unsigned long currentMillis = 0;
+unsigned long previousMillis = 0; 
 
-const char* ssid     = "ESP32-Access-Point";
-const char* password = "123456789";
+const char* ssid     = "NT";
+const char* password = "";
 WiFiServer server(80);
 
 
 void setup()
 {
+    Serial.begin(9600);
     pinMode(15, INPUT_PULLUP);
     status = bme.begin(0x76);
     lcd.begin(16, 2);
     lcd.clear();
     lcd.print ("Welcome!");
     delay(2000);
-    lcd.setCursor(0,1);
-    lcd.print("Setting AP");
-    WiFi.softAP(ssid, password);
-    IPAddress IP = WiFi.softAPIP();
-    lcd.print(String(IP));
-    delay(5000);
+    WiFi.begin(ssid, password);
+    delay(2000);
     lcd.clear();
+    server.begin();
 
 }
  
@@ -41,19 +41,16 @@ void loop(){
   WiFiClient client = server.available(); 
   int button = digitalRead(15);
   lcd.clear();
-  if (client) {                             // If a new client connects,
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
+  if (client) {                           
+    String currentLine = "";                
+    while (client.connected()) {          
+      if (client.available()) {   
+        char c = client.read();            
+        Serial.write(c);                   
         header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
+        if (c == '\n') {                   
           if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
+ 
             client.println("HTTP/1.1 200 OK");
             client.println("Content-type:text/html");
             client.println("Connection: close");
@@ -90,17 +87,15 @@ void loop(){
             client.println("<tr><td>Humidity</td><td><span class=\"sensor\">");
             client.println(bme.readHumidity());
             client.println(" %</span></td></tr>"); 
-            client.println("</body></html>");
-            
-            // The HTTP response ends with another blank line
+            client.println("</body></html>"); 
             client.println();
-            // Break out of the while loop
+       
             break;
-          } else { // if you got a newline, then clear currentLine
+          } else { 
             currentLine = "";
           }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
+        } else if (c != '\r') {  
+          currentLine += c;      
         }
       }
     }
@@ -117,14 +112,14 @@ void loop(){
   }else{
     if(button==LOW){
       counter++;
-      if(counter>=4){
+      if(counter>=5){
         counter = 0;
       }
     }
     lcd.clear();
     prints(counter);
-    delay(1000);
-    }
+    delay(2000);
+  }
 }
 
 void prints(int val){
@@ -141,9 +136,25 @@ void prints(int val){
     case 3:
      printHumidity();
      break;
+    case 4:
+     printIP();
+     break;
   }
 }
+void printIP(){
+  lcd.setCursor(0,0);
+  lcd.print("IP Address:");
+  lcd.setCursor(0,1); 
+  if(WiFi.status() == WL_CONNECTED){
+    IP = WiFi.localIP();
+    lcd.print(IP);
+  }else{
+    String message = "Not connected";
+    lcd.print(message);
+  }
 
+  
+}
 void printTemp(){
     lcd.setCursor(0,0);
     lcd.print("Temperature:");
